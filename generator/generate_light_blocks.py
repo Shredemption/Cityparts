95675bd02a063731bee51ea4f87a5c41133b6085
Shredemption <stijnmichiels2001@gmail.com>
2025-10-14 18:14:35 +0200

    Light block recipes

--- generator/generate_light_blocks.py
+++ generator/generate_light_blocks.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-import blockstates, models, items, loottables
+import blockstates, models, items, loottables, recipes
 
 # === CONFIG ===
 MOD_ID = "streetparts"
@@ -25,9 +25,10 @@ BLOCKSTATE_DIR = os.path.join(BASE_PATH, f"assets/{MOD_ID}/blockstates")
 BLOCK_MODEL_DIR = os.path.join(BASE_PATH, f"assets/{MOD_ID}/models/block")
 ITEM_MODEL_DIR = os.path.join(BASE_PATH, f"assets/{MOD_ID}/models/item")
 LOOT_TABLE_DIR = os.path.join(BASE_PATH, f"data/{MOD_ID}/loot_table/blocks")
+RECIPE_DIR = os.path.join(BASE_PATH, f"data/{MOD_ID}/recipe")
 
 # === Ensure directories exist ===
-for path in [BLOCKSTATE_DIR, BLOCK_MODEL_DIR, ITEM_MODEL_DIR, LOOT_TABLE_DIR]:
+for path in [BLOCKSTATE_DIR, BLOCK_MODEL_DIR, ITEM_MODEL_DIR, LOOT_TABLE_DIR, RECIPE_DIR]:
     os.makedirs(path, exist_ok=True)
 
 
@@ -35,12 +36,14 @@ for path in [BLOCKSTATE_DIR, BLOCK_MODEL_DIR, ITEM_MODEL_DIR, LOOT_TABLE_DIR]:
 for color in COLORS:
     for type in TYPES:
         fullName = f"light_{color}_{type}"
+        lights_tag = "lights"
 
         files = {
             os.path.join(BLOCKSTATE_DIR, f"{fullName}.json"): blockstates.light(color, type),
             os.path.join(BLOCK_MODEL_DIR, f"{fullName}.json"): models.light(color, type),
             os.path.join(ITEM_MODEL_DIR, f"{fullName}.json"): items.light(color, type),
             os.path.join(LOOT_TABLE_DIR, f"{fullName}.json"): loottables.block_drops(fullName),
+            os.path.join(RECIPE_DIR, f"{fullName}.json"): recipes.one_from_tag_stonecutter(lights_tag, fullName),
         }
 
         for path, data in files.items():
