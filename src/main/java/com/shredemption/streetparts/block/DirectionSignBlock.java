ba9be9cfed1cc557a39b0836d6dc0f564f010d69
Shredemption <stijnmichiels2001@gmail.com>
2025-10-18 01:41:50 +0200

    Block interaction added to direction signs

--- src/main/java/com/shredemption/streetparts/block/DirectionSignBlock.java
+++ src/main/java/com/shredemption/streetparts/block/DirectionSignBlock.java
@@ -6,11 +6,19 @@ import com.shredemption.streetparts.blockentity.DirectionSignBlockEntity;
 import com.shredemption.streetparts.template.AttachableHorizontalBlock;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
@@ -31,5 +39,41 @@ public class DirectionSignBlock extends AttachableHorizontalBlock implements Ent
     @Override
     public BlockEntity newBlockEntity(@Nonnull BlockPos pos, @Nonnull BlockState state) {
         return new DirectionSignBlockEntity(pos, state);
+    }
+
+    @Override
+    protected InteractionResult useWithoutItem(@Nonnull BlockState state, @Nonnull Level level, @Nonnull BlockPos pos,
+            @Nonnull Player player,
+            @Nonnull BlockHitResult hitResult) {
+
+        if (!level.isClientSide) {
+            BlockEntity be = level.getBlockEntity(pos);
+            boolean editingFront;
+
+            Direction clickedFace = hitResult.getDirection();
+            Direction blockFacing = state.getValue(DirectionSignBlock.FACING);
+            Direction frontSide = blockFacing.getClockWise();
+
+            editingFront = (clickedFace == frontSide);
+
+            if (be instanceof DirectionSignBlockEntity sign) {
+                player.openTextEdit(sign, editingFront);
+            }
+        }
+        return InteractionResult.sidedSuccess(level.isClientSide);
+    }
+
+    @Override
+    protected ItemInteractionResult useItemOn(@Nonnull ItemStack stack, @Nonnull BlockState state, @Nonnull Level level,
+            @Nonnull BlockPos pos,
+            @Nonnull Player player, @Nonnull InteractionHand hand, @Nonnull BlockHitResult hitResult) {
+
+        InteractionResult result = useWithoutItem(state, level, pos, player, hitResult);
+        return switch (result) {
+            case SUCCESS, CONSUME -> ItemInteractionResult.SUCCESS;
+            case PASS -> ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
+            case FAIL -> ItemInteractionResult.FAIL;
+            default -> ItemInteractionResult.FAIL;
+        };
     }
 }
