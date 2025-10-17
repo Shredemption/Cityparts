08a07a07d652c83aff029f374d858cd66437a9e9
Shredemption <stijnmichiels2001@gmail.com>
2025-10-17 17:53:19 +0200

    AttachableHorizontalBlocks added as class

--- src/main/java/com/shredemption/streetparts/block/TrafficSignBlock.java
+++ src/main/java/com/shredemption/streetparts/block/TrafficSignBlock.java
@@ -1,18 +1,11 @@
 package com.shredemption.streetparts.block;
 
-import javax.annotation.Nonnull;
-
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class TrafficSignBlock extends RotatableHorizontalBlock {
+public class TrafficSignBlock extends AttachableHorizontalBlock {
 
     private static final VoxelShape SignShape = Shapes.or(
             Shapes.box(7 / 16f, 9 / 16f, 1.0f, 9 / 16f, 11 / 16f, 1.375f),
@@ -21,23 +14,5 @@ public class TrafficSignBlock extends RotatableHorizontalBlock {
     public TrafficSignBlock(BlockBehaviour.Properties properties) {
         super(properties, SignShape);
         this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
-    }
-
-    @Override
-    public BlockState updateShape(@Nonnull BlockState state, @Nonnull Direction dir, @Nonnull BlockState neighborState,
-            @Nonnull LevelAccessor level, @Nonnull BlockPos pos, @Nonnull BlockPos neighborPos) {
-        if (dir.getOpposite() == state.getValue(FACING) && !this.canSurvive(state, level, pos)) {
-            return Blocks.AIR.defaultBlockState();
-        }
-        return state;
-    }
-
-    @Override
-    public boolean canSurvive(@Nonnull BlockState state, @Nonnull LevelReader world, @Nonnull BlockPos pos) {
-        Direction direction = state.getValue(FACING);
-        BlockPos supportPos = pos.relative(direction.getOpposite());
-        BlockState suppoertState = world.getBlockState(supportPos);
-
-        return !suppoertState.isAir();
     }
 }
